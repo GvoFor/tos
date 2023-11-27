63ceeeae848d5530bae57cb124a14b026bcc7fb6
Mykhailo Pumnia <misha.pumnya@gmail.com>
2023-11-27 18:22:16 +0200

    Add basic exception classes

---
+++ src/main/java/ua/fam/tos/domain/exceptions/SubmissionException.java
@@ -0,0 +1,13 @@
+package ua.fam.tos.domain.exceptions;
+
+public class SubmissionException extends RuntimeException {
+
+    public SubmissionException() {
+        super();
+    }
+
+    public SubmissionException(String message) {
+        super(message);
+    }
+
+}

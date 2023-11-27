845582c7080d38b0e2c51248ca81f88606cacb0e
Mykhailo Pumnia <misha.pumnya@gmail.com>
2023-11-27 15:49:47 +0200

    Create Spring project stub

---
+++ src/test/java/ua/fam/tos/TosApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.fam.tos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

87907d76a79ccc8ad65f8c14e097695d17c11a69
Mykhailo Pumnia <misha.pumnya@gmail.com>
2023-11-30 09:35:43 +0200

    Add @Service annotation to class BoardService so project can be launched

--- src/main/java/ua/fam/tos/service/BoardService.java
+++ src/main/java/ua/fam/tos/service/BoardService.java
@@ -1,11 +1,13 @@
 package ua.fam.tos.service;
 
+import org.springframework.stereotype.Service;
 import ua.fam.tos.domain.Board;
 import ua.fam.tos.repository.BoardRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class BoardService {
     private final BoardRepository repository;
 

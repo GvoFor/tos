34398b9e587af27aa41d4375a2762132c3ba8798
Kateryna Tsaruk <kat31774782@gmail.com>
2023-11-30 20:07:24 +0200

    Write the SurveyDTO and create SurveyQuestionDTO

--- src/main/java/ua/fam/tos/dto/SurveyDTO.java
+++ src/main/java/ua/fam/tos/dto/SurveyDTO.java
@@ -1,9 +1,44 @@
 package ua.fam.tos.dto;
 
 import ua.fam.tos.domain.boarditem.survey.Survey;
+import ua.fam.tos.domain.boarditem.survey.SurveyStatus;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SurveyDTO {
-    public SurveyDTO(Survey survey){
+    private List<SurveyQuestionDTO> questions;
+    private Map<String, SurveyStatus> submissionStatuses;
 
+    public SurveyDTO(Survey survey) {
+        this.questions = survey.getQuestions().stream()
+                .map(SurveyQuestionDTO::new)
+                .collect(Collectors.toList());
+        this.submissionStatuses = survey.getSubmissionStatuses();
+    }
+
+    public List<SurveyQuestionDTO> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<SurveyQuestionDTO> questions) {
+        this.questions = questions;
+    }
+
+    public Map<String, SurveyStatus> getSubmissionStatuses() {
+        return submissionStatuses;
+    }
+
+    public void setSubmissionStatuses(Map<String, SurveyStatus> submissionStatuses) {
+        this.submissionStatuses = submissionStatuses;
+    }
+
+    public boolean isSubmittedFor(String username) {
+        return submissionStatuses.get(username) == SurveyStatus.SUBMITTED;
+    }
+
+    public boolean isNotSubmittedFor(String username) {
+        return submissionStatuses.get(username) == SurveyStatus.UNSUBMITTED;
     }
 }

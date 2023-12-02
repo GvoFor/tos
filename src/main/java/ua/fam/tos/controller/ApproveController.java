a1a98cd129ff06bc38135d8b4ffbf05e8a207906
Mary Kalashnikova <mdkalashnikova@gmail.com>
2023-12-02 23:41:51 +0200

    Implement of methods approve and decline in ApproveController class

--- src/main/java/ua/fam/tos/controller/ApproveController.java
+++ src/main/java/ua/fam/tos/controller/ApproveController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ua.fam.tos.domain.Board;
+import ua.fam.tos.domain.boarditem.Approvable;
+import ua.fam.tos.domain.boarditem.BoardItem;
+import ua.fam.tos.domain.exceptions.SubmissionException;
 import ua.fam.tos.repository.ContributorRepository;
 import ua.fam.tos.service.BoardService;
 
@@ -28,7 +32,22 @@ public class ApproveController {
                           @PathVariable long itemId,
                           Principal user,
                           RedirectAttributes redirectAttributes) {
-        return "redirect";
+        Board board = boardService.getBoardById(boardId).get();
+        BoardItem item = board.getItemById(itemId).get();
+
+        if (!(item instanceof Approvable approvableItem)) {
+            redirectAttributes.addFlashAttribute("error", "Task is not submittable");
+            return "redirect:/boards/" + boardId + "/items/" + itemId;
+        }
+
+        try {
+            approvableItem.approve(contributorRepository.findContributorByUsername(user.getName()).get());
+        } catch (SubmissionException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+
+
+        return "redirect:/boards/" + boardId + "/items/" + itemId;
     }
 
     @PostMapping("/decline")
@@ -36,7 +55,21 @@ public class ApproveController {
                           @PathVariable long itemId,
                           Principal user,
                           RedirectAttributes redirectAttributes) {
-        return "redirect";
+        Board board = boardService.getBoardById(boardId).get();
+        BoardItem item = board.getItemById(itemId).get();
+
+        if (!(item instanceof Approvable approvableItem)) {
+            redirectAttributes.addFlashAttribute("error", "Task is not submittable");
+            return "redirect:/boards/" + boardId + "/items/" + itemId;
+        }
+
+        try {
+            approvableItem.decline(contributorRepository.findContributorByUsername(user.getName()).get());
+        } catch (SubmissionException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+
+        return "redirect:/boards/" + boardId + "/items/" + itemId;
     }
 
 }

0971aee17e5d3bb0705bbc731ed0e3e4d0762ef8
Mykhailo Pumnia <misha.pumnya@gmail.com>
2023-12-02 20:31:58 +0200

    Add responses for edit and delete requests

--- src/main/java/ua/fam/tos/controller/BoardItemController.java
+++ src/main/java/ua/fam/tos/controller/BoardItemController.java
@@ -2,9 +2,7 @@ package ua.fam.tos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ua.fam.tos.domain.Board;
 import ua.fam.tos.domain.boarditem.BoardItem;
 import ua.fam.tos.domain.boarditem.assignment.Assignment;
@@ -32,8 +30,8 @@ public class BoardItemController {
 
     @GetMapping("/{itemId}")
     public String showBoardItem(Model model, Principal user,
-                            @PathVariable long boardId,
-                            @PathVariable long itemId) {
+                                @PathVariable long boardId,
+                                @PathVariable long itemId) {
         String errorRedirectPath = "redirect:/boards/" + boardId + "/items?error";
 
         Optional<Board> boardOptional = boardService.getBoardById(boardId);
@@ -51,6 +49,8 @@ public class BoardItemController {
         if (!item.isKnowAboutContributorWithUsername(user.getName())) {
             return errorRedirectPath;
         }
+
+        model.addAttribute("username", user.getName());
 
         if (item instanceof Material) {
             model.addAttribute("material",
@@ -79,6 +79,67 @@ public class BoardItemController {
         }
 
         return errorRedirectPath;
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String showBoardItemEditPage(Model model, Principal user,
+                                        @PathVariable long boardId,
+                                        @PathVariable long itemId) {
+        String errorRedirectPath = "redirect:/boards/" + boardId + "/items?error";
+
+        Optional<Board> boardOptional = boardService.getBoardById(boardId);
+        if (boardOptional.isEmpty()) {
+            return errorRedirectPath;
+        }
+        Board board = boardOptional.get();
+
+        Optional<BoardItem> itemOptional = board.getItemById(itemId);
+        if (itemOptional.isEmpty()) {
+            return errorRedirectPath;
+        }
+        BoardItem item = itemOptional.get();
+
+        if (!item.getCreator().getUsername().equals(user.getName())) {
+            return errorRedirectPath;
+        }
+
+        model.addAttribute("username", user.getName());
+
+        if (item instanceof Material) {
+            model.addAttribute("material",
+                    new MaterialDTO((Material) item));
+            return "materialEdit";
+        }
+
+        if (item instanceof Assignment) {
+            model.addAttribute("assignment",
+                    new AssignmentDTO((Assignment) item));
+            return "assignmentEdit";
+        }
+
+
+        if (item instanceof Survey) {
+            model.addAttribute("survey",
+                    new SurveyDTO((Survey) item));
+            return "surveyEdit";
+        }
+
+
+        if (item instanceof ToDoList) {
+            model.addAttribute("todolist",
+                    new ToDoListDTO((ToDoList) item));
+            return "todolistEdit";
+        }
+
+        return errorRedirectPath;
+    }
+
+    @PostMapping("/{itemId}/delete")
+    public String delete(@PathVariable long boardId,
+                         @PathVariable long itemId,
+                         @RequestParam String redirectUrl) {
+        System.out.println("Deleting...");
+        return "redirect:" + redirectUrl;
     }
 
 }

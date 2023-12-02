0f59c8c14ca221a90d93cb9115aad85c9b1fa45d
Mary Kalashnikova <mdkalashnikova@gmail.com>
2023-12-02 23:45:32 +0200

    Implement of methods submit and cancel in SubmitController class

--- src/main/java/ua/fam/tos/controller/SubmitController.java
+++ src/main/java/ua/fam/tos/controller/SubmitController.java
@@ -5,16 +5,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import ua.fam.tos.repository.ContributorRepository;
+import ua.fam.tos.domain.Board;
+import ua.fam.tos.domain.boarditem.BoardItem;
+import ua.fam.tos.domain.boarditem.Submittable;
+import ua.fam.tos.domain.exceptions.SubmissionException;
 import ua.fam.tos.service.BoardService;
+import ua.fam.tos.repository.ContributorRepository;
 
 import java.security.Principal;
 
 @Controller
 @RequestMapping("/boards/{boardId}/items/{itemId}/submittion")
 public class SubmitController {
+
     private final BoardService boardService;
     private final ContributorRepository contributorRepository;
+
     public SubmitController(BoardService service, ContributorRepository contributorRepository) {
         this.boardService = service;
         this.contributorRepository = contributorRepository;
@@ -25,7 +31,20 @@ public class SubmitController {
                          @PathVariable long itemId,
                          Principal user,
                          RedirectAttributes redirectAttributes) {
-        return "redirect";
+        Board board = boardService.getBoardById(boardId).get();
+        BoardItem item = board.getItemById(itemId).get();
+
+        if (!(item instanceof Submittable submittableItem)) {
+            redirectAttributes.addFlashAttribute("error", "Task is not submittable");
+            return "redirect:/boards/" + boardId + "/items/" + itemId;
+        }
+
+        try {
+            submittableItem.submit(contributorRepository.findContributorByUsername(user.getName()).get());
+        } catch (SubmissionException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:/boards/" + boardId + "/items/" + itemId;
     }
 
     @PostMapping("/cancel")
@@ -33,8 +52,20 @@ public class SubmitController {
                          @PathVariable long itemId,
                          Principal user,
                          RedirectAttributes redirectAttributes) {
-        return "redirect";
+        Board board = boardService.getBoardById(boardId).get();
+        BoardItem item = board.getItemById(itemId).get();
+
+        if (!(item instanceof Submittable submittableItem)) {
+            redirectAttributes.addFlashAttribute("error", "Task is not submittable");
+            return "redirect:/boards/" + boardId + "/items/" + itemId;
+        }
+
+        try {
+            submittableItem.cancel(contributorRepository.findContributorByUsername(user.getName()).get());
+        } catch (SubmissionException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:/boards/" + boardId + "/items/" + itemId;
     }
 }
-
 

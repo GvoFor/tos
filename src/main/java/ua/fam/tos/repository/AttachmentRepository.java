e14a52a148ad828fc303accb8ef92a1ac2a05982
Mary Kalashnikova <mdkalashnikova@gmail.com>
2023-12-03 16:17:15 +0200

    Implement of methods getById, deleteById, upload in AttachmentService and save, deleteById in AttachmentRepository

--- src/main/java/ua/fam/tos/repository/AttachmentRepository.java
+++ src/main/java/ua/fam/tos/repository/AttachmentRepository.java
@@ -1,5 +1,17 @@
 package ua.fam.tos.repository;
 
+import org.springframework.web.multipart.MultipartFile;
+import ua.fam.tos.domain.Contributor;
+import ua.fam.tos.domain.boarditem.Attachment;
+
+import java.util.Optional;
+
 public interface AttachmentRepository {
+
+    Optional<Attachment> findById(long boardId, long itemId, long attachmentId);
+
+    void save(long boardId, long itemId, MultipartFile file, Contributor attacher);
+
+    void deleteById(long boardId, long itemId, long attachmentId);
 
 }
